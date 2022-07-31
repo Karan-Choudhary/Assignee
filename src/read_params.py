0d83f910796cfc5db71d1dd341fad84cb698b579
KaranChoudhary <kchoudhary510199@gmail.com>
2022-07-31 10:32:01 +0530

    read_params.py added

---
+++ src/read_params.py
@@ -0,0 +1,16 @@
+import yaml
+import agrparse
+
+def read_params(config_path):
+    with open(config_path, 'r') as stream:
+        try: 
+            params = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return params
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml', hlp='config file')
+    parsed_args = args.parse_args()
+    config = read_params(parsed_args.config)
