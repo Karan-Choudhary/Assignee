7682e3607c9c3a4c041445beff373ce27769916f
KaranChoudhary <kchoudhary510199@gmail.com>
2022-08-15 13:48:22 +0530

    All model levels added

--- src/model/model.py
+++ src/model/model.py
@@ -50,139 +50,139 @@ def build_model(input, instance_norm=True, instance_norm_level_1=False):
         ############################
         # processing: level 4, input size: 28x28
 
-        # conv_l4_d2 = stack(conv_l4_d1, conv_t4a)
-        # conv_l4_d3 = _conv_multi_block(
-        #     conv_l4_d2, 3, num_maps=256, instance_norm=instance_norm)
-        # conv_l4_d4 = _conv_multi_block(
-        #     conv_l4_d3, 3, num_maps=256, instance_norm=instance_norm) + conv_l4_d3
-        # conv_l4_d5 = _conv_multi_block(
-        #     conv_l4_d4, 3, num_maps=256, instance_norm=instance_norm) + conv_l4_d4
-        # conv_l4_d6 = _conv_multi_block(
-        #     conv_l4_d5, 3, num_maps=256, instance_norm=instance_norm)
-        # conv_l4_d7 = stack(conv_l4_d6, conv_t4b)
+        conv_l4_d2 = stack(conv_l4_d1, conv_t4a)
+        conv_l4_d3 = _conv_multi_block(
+            conv_l4_d2, 3, num_maps=256, instance_norm=instance_norm)
+        conv_l4_d4 = _conv_multi_block(
+            conv_l4_d3, 3, num_maps=256, instance_norm=instance_norm) + conv_l4_d3
+        conv_l4_d5 = _conv_multi_block(
+            conv_l4_d4, 3, num_maps=256, instance_norm=instance_norm) + conv_l4_d4
+        conv_l4_d6 = _conv_multi_block(
+            conv_l4_d5, 3, num_maps=256, instance_norm=instance_norm)
+        conv_l4_d7 = stack(conv_l4_d6, conv_t4b)
 
-        # conv_l4_d8 = _conv_multi_block(
-        #     conv_l4_d7, 3, num_maps=256, instance_norm=instance_norm)
+        conv_l4_d8 = _conv_multi_block(
+            conv_l4_d7, 3, num_maps=256, instance_norm=instance_norm)
 
-        # conv_t3a = _conv_transpose_layer(conv_l4_d8, 128, 3, 2)  # 28 to 56
-        # conv_t3b = _conv_transpose_layer(conv_l4_d8, 128, 3, 2)  # 28 to 56
+        conv_t3a = _conv_transpose_layer(conv_l4_d8, 128, 3, 2)  # 28 to 56
+        conv_t3b = _conv_transpose_layer(conv_l4_d8, 128, 3, 2)  # 28 to 56
 
-        # # output: level 4
-        # conv_l4_out = _conv_layer(
-        #     conv_l4_d8, 3, 3, 1, relu=False, instance_norm=False)
-        # output_l4 = tf.nn.tanh(conv_l4_out) * 0.58 + 0.58
+        # output: level 4
+        conv_l4_out = _conv_layer(
+            conv_l4_d8, 3, 3, 1, relu=False, instance_norm=False)
+        output_l4 = tf.nn.tanh(conv_l4_out) * 0.58 + 0.58
 
-        # ############################
-        # # processing: level 3, input size: 56x56
+        ############################
+        # processing: level 3, input size: 56x56
 
-        # conv_l3_d2 = stack(conv_l3_d1, conv_t3a)
-        # conv_l3_d3 = _conv_multi_block(
-        #     conv_l3_d2, 5, num_maps=128, instance_norm=instance_norm) + conv_l3_d2
-        # # conv_l3_d4 = stack(conv_l3_d3, conv_l3_d2)
-        # conv_l3_d4 = _conv_multi_block(
-        #     conv_l3_d4, 5, num_maps=128, instance_norm=instance_norm) + conv_l3_d3
-        # # conv_l3_d6 = stack(conv_l3_d5, conv_l3_d4)
-        # conv_l3_d5 = _conv_multi_block(
-        #     conv_l3_d6, 5, num_maps=128, instance_norm=instance_norm) + conv_l3_d4
-        # # conv_l3_d8 = stack(conv_l3_d7, conv_l3_d6)
-        # conv_l3_d6 = _conv_multi_block(
-        #     conv_l3_d5, 5, num_maps=128, instance_norm=instance_norm)
+        conv_l3_d2 = stack(conv_l3_d1, conv_t3a)
+        conv_l3_d3 = _conv_multi_block(
+            conv_l3_d2, 5, num_maps=128, instance_norm=instance_norm) + conv_l3_d2
+        # conv_l3_d4 = stack(conv_l3_d3, conv_l3_d2)
+        conv_l3_d4 = _conv_multi_block(
+            conv_l3_d4, 5, num_maps=128, instance_norm=instance_norm) + conv_l3_d3
+        # conv_l3_d6 = stack(conv_l3_d5, conv_l3_d4)
+        conv_l3_d5 = _conv_multi_block(
+            conv_l3_d6, 5, num_maps=128, instance_norm=instance_norm) + conv_l3_d4
+        # conv_l3_d8 = stack(conv_l3_d7, conv_l3_d6)
+        conv_l3_d6 = _conv_multi_block(
+            conv_l3_d5, 5, num_maps=128, instance_norm=instance_norm)
 
-        # conv_l3_d7 = stack(stack(conv_l3_d6, conv_l3_d1), conv_t3b)
+        conv_l3_d7 = stack(stack(conv_l3_d6, conv_l3_d1), conv_t3b)
 
-        # conv_l3_d8 = _conv_multi_block(
-        #     conv_l3_d7, 3, num_maps=128, instance_norm=instance_norm)
+        conv_l3_d8 = _conv_multi_block(
+            conv_l3_d7, 3, num_maps=128, instance_norm=instance_norm)
 
-        # conv_t2a = _conv_transpose_layer(conv_l3_d8, 64, 3, 2)  # 56 to 112
-        # conv_t2b = _conv_transpose_layer(conv_l3_d8, 64, 3, 2)  # 56 to 112
+        conv_t2a = _conv_transpose_layer(conv_l3_d8, 64, 3, 2)  # 56 to 112
+        conv_t2b = _conv_transpose_layer(conv_l3_d8, 64, 3, 2)  # 56 to 112
 
-        # # output: level 3
-        # conv_l3_out = _conv_layer(
-        #     conv_l3_d8, 3, 3, 1, relu=False, instance_norm=False)
-        # output_l3 = tf.nn.tanh(conv_l3_out) * 0.58 + 0.58
+        # output: level 3
+        conv_l3_out = _conv_layer(
+            conv_l3_d8, 3, 3, 1, relu=False, instance_norm=False)
+        output_l3 = tf.nn.tanh(conv_l3_out) * 0.58 + 0.58
 
-        # ############################
-        # # processing: level 2, input size: 112x112
-        # conv_l2_d2 = stack(conv_l2_d1, conv_t2a)
-        # conv_l2_d3 = _conv_multi_block(
-        #     conv_l2_d2, 5, num_maps=64, instance_norm=instance_norm)
-        # conv_l2_d4 = stack(conv_l2_d3, conv_l2_d1)
+        ############################
+        # processing: level 2, input size: 112x112
+        conv_l2_d2 = stack(conv_l2_d1, conv_t2a)
+        conv_l2_d3 = _conv_multi_block(
+            conv_l2_d2, 5, num_maps=64, instance_norm=instance_norm)
+        conv_l2_d4 = stack(conv_l2_d3, conv_l2_d1)
 
-        # conv_l2_d5 = _conv_multi_block(
-        #     conv_l2_d4, 7, num_maps=64, instance_norm=instance_norm) + conv_l2_d4
-        # conv_l2_d6 = _conv_multi_block(
-        #     conv_l2_d5, 7, num_maps=64, instance_norm=instance_norm) + conv_l2_d5
-        # conv_l2_d7 = _conv_multi_block(
-        #     conv_l2_d6, 7, num_maps=64, instance_norm=instance_norm) + conv_l2_d6
-        # conv_l2_d8 = _conv_multi_block(
-        #     conv_l2_d7, 7, num_maps=64, instance_norm=instance_norm)
+        conv_l2_d5 = _conv_multi_block(
+            conv_l2_d4, 7, num_maps=64, instance_norm=instance_norm) + conv_l2_d4
+        conv_l2_d6 = _conv_multi_block(
+            conv_l2_d5, 7, num_maps=64, instance_norm=instance_norm) + conv_l2_d5
+        conv_l2_d7 = _conv_multi_block(
+            conv_l2_d6, 7, num_maps=64, instance_norm=instance_norm) + conv_l2_d6
+        conv_l2_d8 = _conv_multi_block(
+            conv_l2_d7, 7, num_maps=64, instance_norm=instance_norm)
 
-        # conv_l2_d9 = stack(conv_l2_d8, conv_l2_d1)
+        conv_l2_d9 = stack(conv_l2_d8, conv_l2_d1)
 
-        # conv_l2_d10 = _conv_multi_block(
-        #     conv_l2_d9, 5, num_maps=64, instance_norm=instance_norm)
+        conv_l2_d10 = _conv_multi_block(
+            conv_l2_d9, 5, num_maps=64, instance_norm=instance_norm)
 
-        # conv_l2_d11 = stack(conv_l2_d10, conv_t2b)
+        conv_l2_d11 = stack(conv_l2_d10, conv_t2b)
 
-        # conv_l2_d12 = _conv_multi_block(
-        #     conv_l2_d11, 3, num_maps=64, instance_norm=instance_norm)
+        conv_l2_d12 = _conv_multi_block(
+            conv_l2_d11, 3, num_maps=64, instance_norm=instance_norm)
 
-        # conv_t1a = _conv_transpose_layer(conv_l2_d12, 32, 3, 2)  # 112 to 224
-        # conv_t1b = _conv_transpose_layer(conv_l2_d12, 32, 3, 2)  # 112 to 224
+        conv_t1a = _conv_transpose_layer(conv_l2_d12, 32, 3, 2)  # 112 to 224
+        conv_t1b = _conv_transpose_layer(conv_l2_d12, 32, 3, 2)  # 112 to 224
 
-        # # output: level 2
-        # conv_l2_out = _conv_layer(
-        #     conv_l2_d12, 3, 3, 1, relu=False, instance_norm=False)
-        # output_l2 = tf.nn.tanh(conv_l2_out) * 0.58 + 0.58
+        # output: level 2
+        conv_l2_out = _conv_layer(
+            conv_l2_d12, 3, 3, 1, relu=False, instance_norm=False)
+        output_l2 = tf.nn.tanh(conv_l2_out) * 0.58 + 0.58
 
-        # ############################
-        # # processing: level 1, input size: 224x224
+        ############################
+        # processing: level 1, input size: 224x224
 
-        # conv_l1_d2 = stack(conv_l1_d1, conv_t1a)
-        # conv_l1_d3 = _conv_multi_block(
-        #     conv_l1_d2, 5, num_maps=32, instance_norm=False)
-        # conv_l1_d4 = stack(conv_l1_d3, conv_l1_d1)
-        # conv_l1_d5 = _conv_multi_block(
-        #     conv_l1_d4, 7, num_maps=32, instance_norm=False)
+        conv_l1_d2 = stack(conv_l1_d1, conv_t1a)
+        conv_l1_d3 = _conv_multi_block(
+            conv_l1_d2, 5, num_maps=32, instance_norm=False)
+        conv_l1_d4 = stack(conv_l1_d3, conv_l1_d1)
+        conv_l1_d5 = _conv_multi_block(
+            conv_l1_d4, 7, num_maps=32, instance_norm=False)
 
-        # conv_l1_d6 = _conv_multi_block(
-        #     conv_l1_d5, 9, num_maps=32, instance_norm=instance_norm_level_1)
-        # conv_l1_d7 = _conv_multi_block(
-        #     conv_l1_d6, 9, num_maps=32, instance_norm=instance_norm_level_1) + conv_l1_d6
-        # conv_l1_d8 = _conv_multi_block(
-        #     conv_l1_d7, 9, num_maps=32, instance_norm=instance_norm_level_1) + conv_l1_d7
-        # conv_l1_d9 = _conv_multi_block(
-        #     conv_l1_d8, 9, num_maps=32, instance_norm=instance_norm_level_1) + conv_l1_d8
+        conv_l1_d6 = _conv_multi_block(
+            conv_l1_d5, 9, num_maps=32, instance_norm=instance_norm_level_1)
+        conv_l1_d7 = _conv_multi_block(
+            conv_l1_d6, 9, num_maps=32, instance_norm=instance_norm_level_1) + conv_l1_d6
+        conv_l1_d8 = _conv_multi_block(
+            conv_l1_d7, 9, num_maps=32, instance_norm=instance_norm_level_1) + conv_l1_d7
+        conv_l1_d9 = _conv_multi_block(
+            conv_l1_d8, 9, num_maps=32, instance_norm=instance_norm_level_1) + conv_l1_d8
 
-        # conv_l1_d10 = _conv_multi_block(
-        #     conv_l1_d9, 7, num_maps=32, instance_norm=False)
+        conv_l1_d10 = _conv_multi_block(
+            conv_l1_d9, 7, num_maps=32, instance_norm=False)
 
-        # conv_l1_d11 = stack(conv_l1_d10, conv_l1_d1)
+        conv_l1_d11 = stack(conv_l1_d10, conv_l1_d1)
 
-        # conv_l1_d12 = _conv_multi_block(
-        #     conv_l1_d11, 5, num_maps=32, instance_norm=False)
+        conv_l1_d12 = _conv_multi_block(
+            conv_l1_d11, 5, num_maps=32, instance_norm=False)
 
-        # conv_l1_d13 = stack(stack(conv_l1_d12, conv_t1b), conv_l1_d1)
+        conv_l1_d13 = stack(stack(conv_l1_d12, conv_t1b), conv_l1_d1)
 
-        # conv_l1_d14 = _conv_multi_block(
-        #     conv_l1_d13, 3, num_maps=32, instance_norm=False)
+        conv_l1_d14 = _conv_multi_block(
+            conv_l1_d13, 3, num_maps=32, instance_norm=False)
 
-        # # output: level 1
-        # conv_l1_out = _conv_layer(
-        #     conv_l1_d14, 3, 3, 1, relu=False, instance_norm=False)
-        # output_l1 = tf.nn.tanh(conv_l1_out) * 0.58 + 0.58
+        # output: level 1
+        conv_l1_out = _conv_layer(
+            conv_l1_d14, 3, 3, 1, relu=False, instance_norm=False)
+        output_l1 = tf.nn.tanh(conv_l1_out) * 0.58 + 0.58
 
-        # ############################
-        # # Processing: level 0 (2x Upscaling), input size: 224x224
+        ############################
+        # Processing: level 0 (2x Upscaling), input size: 224x224
 
-        # conv_l0 = _conv_transpose_layer(conv_l1_d14, 8, 3, 2)   # 224 to 448
-        # conv_l0_out = _conv_layer(
-        #     conv_l0, 3, 3, 1, relu=False, instance_norm=False)
+        conv_l0 = _conv_transpose_layer(conv_l1_d14, 8, 3, 2)   # 224 to 448
+        conv_l0_out = _conv_layer(
+            conv_l0, 3, 3, 1, relu=False, instance_norm=False)
 
-        # output_l0 = tf.nn.tanh(conv_l0_out) * 0.58 + 0.5
+        output_l0 = tf.nn.tanh(conv_l0_out) * 0.58 + 0.5
 
-        # return output_l0, output_l1, output_l2, output_l3, output_l4, output_l5
-        return output_l5
+        return output_l0, output_l1, output_l2, output_l3, output_l4, output_l5
+        # return output_l5
 
 
 def stack(x, y):
@@ -308,8 +308,8 @@ if __name__ == '__main__':
 
     phone_ = tf.keras.layers.Input(shape=(224, 224, 4), batch_size=1, dtype=tf.float32)
     print(phone_.shape)
-    # output_l0, output_l1, output_l2, output_l3, output_l4, output_l5 = build_model(phone_, instance_norm=True, instance_norm_level_1=False)
-    output_l5 = build_model(phone_, instance_norm=True, instance_norm_level_1=False)
-    # model = tf.keras.Model(inputs=phone_, outputs=output_l0, name='Assignee')
-    model = tf.keras.Model(inputs=phone_, outputs=output_l5, name='Assignee')
+    output_l0, output_l1, output_l2, output_l3, output_l4, output_l5 = build_model(phone_, instance_norm=True, instance_norm_level_1=False)
+    # output_l5 = build_model(phone_, instance_norm=True, instance_norm_level_1=False)
+    model = tf.keras.Model(inputs=phone_, outputs=output_l0, name='Assignee')
+    # model = tf.keras.Model(inputs=phone_, outputs=output_l5, name='Assignee')
     print("Successfully built model")
